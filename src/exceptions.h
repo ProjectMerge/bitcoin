9ec15f7bd8484fff972dc93f4a3bd5194b4f6ae7
barrystyle <barrystyle@westnet.com.au>
2020-08-16 03:44:55 +0800

    remove strange header reference causing leveldb include to be skipped for win64 build

--- src/exceptions.h
+++ src/exceptions.h
@@ -3,7 +3,6 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include <util/system.h>
-#include <validation.h>
 
 void initVectors();
 bool isExceptionBlock(uint256& hash);
