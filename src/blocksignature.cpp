7baf90e887ad4a03d88bf0a1c34f66da4a1edb25
barrystyle <barrystyle@westnet.com.au>
2020-08-20 20:53:40 +0800

    amend copyright notices where appropriate

--- src/blocksignature.cpp
+++ src/blocksignature.cpp
@@ -1,5 +1,5 @@
 // Copyright (c) 2017-2018 The PIVX developers
-// Copyright (c) 2020 The MERGE developers
+// Copyright (c) 2018-2020 The Merge Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
